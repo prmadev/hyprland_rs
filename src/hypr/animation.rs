b9552a55a97dc0968e358c477ea4d28a841eae1a
amirography <amirhossein.alesheikh@gmail.com>
2022-10-30 11:50:21 +0330

    feat():implemented animation fields

--- src/hypr/animation.rs
+++ src/hypr/animation.rs
@@ -1,20 +1,19 @@
-use std::fmt;
-
 #[derive(Clone)]
-pub struct Animation {}
+pub struct Animation {
+    /// enable animations
+    enabled: bool,
+}
 impl Animation {
     pub fn new() -> Animation {
-        Animation {}
+        Animation { enabled: true }
     }
 
     pub fn run_commands(&mut self) -> Vec<String> {
         const PREFIX: &str = "keyword";
         const SECTION: &str = "animation";
-        vec![
-           // format!(
-           //     "{PREFIX} {SECTION}:{} {}",
-           //     "border_size\n", self.border_size
-           // ),
-        ]
+        vec![format!("{PREFIX} {SECTION}:{} {}", "enabled", self.enabled,)]
+    }
+    pub fn set_enabled(&mut self, new_value: bool) {
+        self.enabled = new_value
     }
 }
